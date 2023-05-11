45fd7791522fd13062f91fefb76e8a76281d2f9e
Sayan Ghosh <99132893+sayanghoshofficial@users.noreply.github.com>
2023-05-11 18:23:22 +0530

    create javaScript

---
+++ Script.js
@@ -0,0 +1,22 @@
+// Dom Elements
+const boxs = document.querySelectorAll(".drag-box"),
+  img = document.querySelector("#img-box");
+
+//   Loop through each boxes element
+
+boxs.forEach((box) => {
+  // when a draggable element dragged over a box element
+  box.addEventListener("dragover", (e) => {
+    e.preventDefault(); // prevent default behaviour
+    box.classList.add("hovered");
+  });
+  // when draggable element leaves from a box element
+  box.addEventListener("dragleave", (e) => {
+    box.classList.remove("hovered");
+  });
+  // when draggable element droppped on a box
+  box.addEventListener("drop", () => {
+    box.appendChild(img);
+    box.classList.remove("hovered");
+  });
+});
